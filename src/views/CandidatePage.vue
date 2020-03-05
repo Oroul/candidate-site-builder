75553ad60fb815a784ae4ad2637c773738da5772
Austen Bouza <austen.bouza@gmail.com>
2020-03-05 15:41:33 -0600

    Add styling for 404 page and add redirect
    Adds css styling for candidates that do not have an entry in
    candidates.json. Includes a button to redirect to the Google form for
    new candidates.

--- src/views/CandidatePage.vue
+++ src/views/CandidatePage.vue
@@ -3,13 +3,22 @@
     <div v-if="twitterIds.includes($route.params.id)">
       <Candidate v-bind:candidate="getCandidate($route.params.id)" />
     </div>
-    <div v-else>
+    <div v-else id="candidate-404">
+      <div id="question-mark">
+        <div>?</div>
+      </div>
       <h3>No candidate found!</h3>
       <p>
         There is no candidate associated with this Twitter handle. If you would
         like an automated website for this candidate, please fill out the form
-        <a href="#">here</a>
+        below.
       </p>
+      <b-button
+        class="btn btn-info btn-xl"
+        href="https://docs.google.com/forms/d/e/1FAIpQLSecum17d1BhF_PBkhis6oKiw5oEAIW_VE4uepcipaKNG4ej0g/viewform"
+      >
+        New Candidate Form
+      </b-button>
     </div>
   </div>
 </template>
@@ -40,3 +49,27 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+#candidate-404 {
+  display: grid;
+  justify-items: center;
+  padding-top: 2em;
+}
+
+#question-mark {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 10em;
+  background: #ddd;
+  width: 1.5em;
+  height: 1.5em;
+  border-radius: 50%;
+  margin-bottom: 0.2em;
+}
+
+p {
+  padding: 1.5em;
+}
+</style>
