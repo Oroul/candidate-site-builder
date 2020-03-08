77a3fb2e27b8cd80adbcb268d1c64ed648addfa6
Austen Bouza <austen.bouza@gmail.com>
2020-03-08 15:30:12 -0600

    Fix 'npm run pull' command for candidate data
    Command now correctly updates candidate data. This command is necessary
    to trigger netlify build hooks that update candidate data without
    requiring a full redeploy of the application.
    
    Newline stripping has also been added for the candidate bio section, and
    the script will now generate output describing all candidates that have
    had their information updated.

--- load-candidates.js
+++ load-candidates.js
@@ -7,12 +7,11 @@ const url =
 
 const get_candidates = async () => {
   const csvFile = await fetch(url).then(r => r.text());
-  //console.log(csvFile);
   const records = parse(csvFile, { columns: false });
   const jsonData = records.map(r => ({
     twitterId: r[0],
     fullName: r[1],
-    bio: r[2],
+    bio: r[2].replace(/(\r\n|\n|\r)/gm," "),
     keywords: r[3],
     email: r[4],
     phone: r[5],
@@ -21,6 +20,12 @@ const get_candidates = async () => {
   }));
 
   fs.writeFileSync("./src/candidates.json", JSON.stringify(jsonData));
+  console.log(`Wrote data for ${records.length} candidates:`);
+  for (let c of jsonData) {
+    console.log(
+      `${c.fullName} | Twitter: ${c.twitterId} | Email: ${c.email} | Phone: ${c.phone} | Facebook: ${c.facebookId} | \nBio: ${c.bio} | \n Keywords: ${c.keywords} | \nPledge ${c.pledge} \n-------------------------`
+    );
+  }
 };
 
 get_candidates();
